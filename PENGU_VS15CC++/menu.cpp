5ba1dfc17b88dfbacc7524017f637b1937d24b50
Denis <roma4004@yandex.ru>
2015-12-16 20:08:00 +0200

    Create project, add winForm C++

---
+++ PENGU_VS15CC++/menu.cpp
@@ -0,0 +1,14 @@
+#include "menu.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	PENGU::menu form;
+	Application::Run(%form);
+}
