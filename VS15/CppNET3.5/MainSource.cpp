d058056ed6c3c476bc98d71d18d4eead9f4e484a
Denis <roma4004@yandex.ru>
2016-01-06 21:54:31 +0200

    add dll, fonts/, func DrawText and remove texture/

--- VS15/CppNET3.5/MainSource.cpp
+++ VS15/CppNET3.5/MainSource.cpp
@@ -1,23 +1,33 @@
 #include <SFML/Graphics.hpp>
-#include <Box2D/Box2D.h>
-#include "MainSource.h"
-
-int main(){
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+#include <Box2D/Box2D.h>  
+#include "MainSource.h"	 
 	
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
+int main(){		
+	CircleShape shape(100.f);
+	shape.setFillColor(Color::Green);
+	while (window.isOpen() ){
+		Event event;
+		while (window.pollEvent(event)){
+			if (event.type == Event::Closed) window.close();
 		}
 		window.clear();
-		window.draw(shape);
+		window.draw(shape);	
+		
+		DrawText(18, 20, 100, L"Выводимый текст");
+		
 		window.display();
 	}
 	return 0;
 }
+									 	
+void DrawText(int fontSize, float posX, float posY, String setText, 
+			  Color colorOfText, String FontFamily){
+	Font font; String FontExt = ".ttf";
+	font.loadFromFile("Fonts/"+FontFamily+FontExt);
+	Text text("", font, fontSize);
+	text.setColor(colorOfText);
+	text.setStyle(sf::Text::Bold);
+	text.setString(setText);
+	text.setPosition(posX, posY);
+	window.draw(text);
+};
--- VS15/CppNET3.5/MainSource.h
+++ VS15/CppNET3.5/MainSource.h
@@ -1,1 +1,8 @@
 #pragma once
+
+using namespace sf;
+
+void DrawText(int fontSize, float posX, float posY, String setText,
+	Color colorOfText = Color::Black, String FontFamily = "times");
+
+RenderWindow window(VideoMode(200, 200), "SFML works!");
